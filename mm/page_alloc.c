8408d5c725d70a0eb8839558b7192adb737d509d
Andrey Konovalov <adech.fo@gmail.com>
2014-08-07 15:05:06 +0400

    Intercept some splinlock and pagealloc functions

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -61,6 +61,7 @@
 #include <linux/page-debug-flags.h>
 #include <linux/hugetlb.h>
 #include <linux/sched/rt.h>
+#include <linux/tsan.h>
 
 #include <asm/sections.h>
 #include <asm/tlbflush.h>
@@ -747,6 +748,7 @@ static bool free_pages_prepare(struct page *page, unsigned int order)
 
 	trace_mm_page_free(page, order);
 	kmemcheck_free_shadow(page, order);
+	tsan_free_page(page, order);
 
 	if (PageAnon(page))
 		page->mapping = NULL;
@@ -1477,6 +1479,8 @@ void split_page(struct page *page, unsigned int order)
 	if (kmemcheck_page_is_tracked(page))
 		split_page(virt_to_page(page[0].shadow), order);
 #endif
+
+	tsan_split_page(page, order);
 
 	for (i = 1; i < (1 << order); i++)
 		set_page_refcounted(page + i);
@@ -2714,6 +2718,7 @@ nopage:
 got_pg:
 	if (kmemcheck_enabled)
 		kmemcheck_pagealloc_alloc(page, order, gfp_mask);
+	tsan_alloc_page(page, order, gfp_mask);
 
 	return page;
 }
