e53e4be2ecf8289476890629d4c708413b167115
Andrey Konovalov <adech.fo@gmail.com>
2014-08-14 11:14:00 +0400

    slab_alloc/slab_free hooks

--- mm/ktsan/malloc.c
+++ mm/ktsan/malloc.c
@@ -1,4 +1,13 @@
 #include "ktsan.h"
 
-/* kmalloc/kfree hooks */
+#include <linux/slab.h>
 
+/* TODO: kmalloc/kfree hooks? */
+
+void ktsan_slab_alloc(struct kmem_cache *cache, void *obj)
+{
+}
+
+void ktsan_slab_free(struct kmem_cache *cache, void *obj)
+{
+}
