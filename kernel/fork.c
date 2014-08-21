f5d5e449436c4cde10e71578248617ad475ce441
Andrey Konovalov <adech.fo@gmail.com>
2014-08-21 19:35:27 +0400

    Fixed hooks ktsan_thread_start/stop

--- kernel/fork.c
+++ kernel/fork.c
@@ -1729,6 +1729,8 @@ long _do_fork(unsigned long clone_flags,
 			get_task_struct(p);
 		}
 
+		ktsan_thr_create(&p->ktsan, p->pid);
+
 		wake_up_new_task(p);
 
 		/* forking complete and child started to run, tell ptracer */
