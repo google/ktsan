77de3dd7087993c3657e79bc7c57f7df9a96443d
Andrey Konovalov <adech.fo@gmail.com>
2014-08-29 16:10:34 +0400

    Simplify internal allocator

--- mm/ktsan/alloc.c
+++ mm/ktsan/alloc.c
@@ -7,76 +7,39 @@
 #include <linux/mm_types.h>
 #include <linux/spinlock.h>
 
-struct kt_cache_obj_s {
-	int link;
-};
-
-typedef struct kt_cache_obj_s kt_cache_obj_t;
-
-#define INDEX_TO_ADDR(n) (cache->addr + (n) * cache->obj_size)
-#define INDEX_TO_OBJ(n) ((kt_cache_obj_t *)INDEX_TO_ADDR(n))
-#define ADDR_TO_INDEX(addr) (((addr) - cache->addr) / cache->obj_size)
-
-/* Only available during early boot. */
-static uptr_t alloc_memory(uptr_t size)
-{
-	uptr_t found;
-
-	found = memblock_alloc(size, PAGE_SIZE);
-	BUG_ON(found == 0);
-	return (uptr_t)(phys_to_virt(found));
-}
-
-/* Only available during early boot. */
-static void free_memory(uptr_t addr, uptr_t size)
-{
-	int rv;
-
-	rv = memblock_free(addr, size);
-	BUG_ON(rv == 0);
-}
-
 /* Only available during early boot. */
 void __init kt_cache_init(kt_cache_t *cache, size_t obj_size,
 			  size_t obj_max_num)
 {
+	phys_addr_t paddr;
+	uptr_t obj;
 	int i;
 
-	cache->obj_size = round_up(obj_size, sizeof(unsigned long));
-	cache->obj_max_num = obj_max_num;
-	cache->obj_num = 0;
+	BUG_ON(obj_size == 0);
+	BUG_ON(obj_max_num == 0);
 
-	cache->space = cache->obj_size * cache->obj_max_num;
-	cache->addr = alloc_memory(cache->space);
+	obj_size = round_up(obj_size, sizeof(unsigned long));
+	cache->mem_size = obj_size * obj_max_num;
 
-	BUG_ON(cache->obj_max_num == 0);
+	paddr = memblock_alloc(cache->mem_size, PAGE_SIZE);
+	BUG_ON(paddr == 0);
+	cache->base = (uptr_t)(phys_to_virt(paddr));
 
-	for (i = 0; i < cache->obj_max_num - 1; i++)
-		INDEX_TO_OBJ(i)->link = i + 1;
-	INDEX_TO_OBJ(cache->obj_max_num - 1)->link = -1;
+	for (i = 0, obj = cache->base; i < obj_max_num-1; i++, obj += obj_size)
+		*(void **)obj = (void *)(obj + obj_size);
+	*(void **)obj = NULL;
 
-	cache->head = 0;
+	cache->head = (void *)cache->base;
 	spin_lock_init(&cache->lock);
 }
 
 /* Only available during early boot. */
 void __init kt_cache_destroy(kt_cache_t *cache)
 {
-	free_memory(cache->addr, cache->space);
-}
+	int rv;
 
-static void *kt_cache_alloc_impl(kt_cache_t *cache)
-{
-	void *obj;
-
-	if (cache->head == -1)
-		return NULL;
-
-	obj = (void *)INDEX_TO_ADDR(cache->head);
-	cache->head = INDEX_TO_OBJ(cache->head)->link;
-	cache->obj_num++;
-
-	return obj;
+	rv = memblock_free(cache->base, cache->mem_size);
+	BUG_ON(rv == 0);
 }
 
 void *kt_cache_alloc(kt_cache_t *cache)
@@ -84,47 +47,18 @@ void *kt_cache_alloc(kt_cache_t *cache)
 	void *obj;
 
 	spin_lock(&cache->lock);
-
-	BUG_ON(cache->head < -1);
-	BUG_ON(cache->head >= cache->obj_max_num);
-
-	obj = kt_cache_alloc_impl(cache);
-
-	BUG_ON(cache->head < -1);
-	BUG_ON(cache->head >= cache->obj_max_num);
-
+	obj = cache->head;
+	if (obj)
+		cache->head = *(void **)obj;
 	spin_unlock(&cache->lock);
 
 	return obj;
 }
 
-static void kt_cache_free_impl(kt_cache_t *cache, uptr_t addr)
-{
-	int index;
-
-	index = ADDR_TO_INDEX(addr);
-	INDEX_TO_OBJ(index)->link = cache->head;
-	cache->head = index;
-	cache->obj_num--;
-}
-
 void kt_cache_free(kt_cache_t *cache, void *obj)
 {
-	uptr_t addr = (uptr_t)obj;
-
 	spin_lock(&cache->lock);
-
-	BUG_ON(cache->head < -1);
-	BUG_ON(cache->head >= cache->obj_max_num);
-
-	BUG_ON(addr < cache->addr);
-	BUG_ON(addr >= cache->addr + cache->space);
-	BUG_ON((addr - cache->addr) % cache->obj_size != 0);
-
-	kt_cache_free_impl(cache, addr);
-
-	BUG_ON(cache->head < -1);
-	BUG_ON(cache->head >= cache->obj_max_num);
-
+	*(void **)obj = cache->head;
+	cache->head = obj;
 	spin_unlock(&cache->lock);
 }
