bb2b913fdb60d39e5f7bc8d5b7394bff986711ae
Andrey Konovalov <adech.fo@gmail.com>
2014-09-02 14:34:51 +0400

    Fix use-after free in kt_memblock_free

--- mm/ktsan/memblock.c
+++ mm/ktsan/memblock.c
@@ -8,28 +8,34 @@ void kt_memblock_alloc(kt_thr_t *thr, uptr_t pc, uptr_t addr, size_t size)
 void kt_memblock_free(kt_thr_t *thr, uptr_t pc, uptr_t addr, size_t size)
 {
 	kt_tab_memblock_t *memblock;
-	kt_tab_sync_t *sync;
+	kt_tab_sync_t *sync, *next;
 
 	memblock = kt_tab_access(&kt_ctx.memblock_tab, addr, NULL, true);
 
 	if (memblock == NULL)
 		return;
 
-	for (sync = memblock->head; sync; sync = sync->next) {
-		sync = kt_tab_access(&kt_ctx.sync_tab,
-			sync->tab.key, NULL, true);
-		BUG_ON(sync == NULL);
-
-		spin_unlock(&sync->tab.lock);
-		kt_cache_free(&kt_ctx.sync_tab.obj_cache, sync);
-
-		kt_stat_dec(thr, kt_stat_sync_objects);
-		kt_stat_inc(thr, kt_stat_sync_free);
-	}
+	sync = memblock->head;
 
 	spin_unlock(&memblock->tab.lock);
 	kt_cache_free(&kt_ctx.memblock_tab.obj_cache, memblock);
 
 	kt_stat_dec(thr, kt_stat_memblock_objects);
 	kt_stat_inc(thr, kt_stat_memblock_free);
+
+	while (sync) {
+		sync = kt_tab_access(&kt_ctx.sync_tab,
+			sync->tab.key, NULL, true);
+		BUG_ON(sync == NULL);
+
+		next = sync->next;
+
+		spin_unlock(&sync->tab.lock);
+		kt_cache_free(&kt_ctx.sync_tab.obj_cache, sync);
+
+		kt_stat_dec(thr, kt_stat_sync_objects);
+		kt_stat_inc(thr, kt_stat_sync_free);
+
+		sync = next;
+	}
 }
