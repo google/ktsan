f3d52638b0293c641275217376df3522d4b16b54
Andrey Konovalov <adech.fo@gmail.com>
2014-09-02 18:33:39 +0400

    Hook atomic_read and atomic_set

--- include/linux/ktsan.h
+++ include/linux/ktsan.h
@@ -31,6 +31,9 @@ void ktsan_mtx_pre_lock(void *addr, bool write, bool try);
 void ktsan_mtx_post_lock(void *addr, bool write, bool try);
 void ktsan_mtx_pre_unlock(void *addr, bool write);
 
+int ktsan_atomic32_read(const void *addr);
+void ktsan_atomic32_set(void *addr, int value);
+
 void ktsan_read1(void *addr);
 void ktsan_read2(void *addr);
 void ktsan_read4(void *addr);
@@ -58,10 +61,13 @@ struct ktsan_thr_s {
 static inline void ktsan_init_early(void) {}
 static inline void ktsan_init(void) {}
 
-static inline void ktsan_thr_create(ktsan_thr_t *new, int tid) {}
+static inline void ktsan_thr_create(struct ktsan_thr_s *new, int tid) {}
 static inline void ktsan_thr_finish(void) {}
 static inline void ktsan_thr_start(void) {}
 static inline void ktsan_thr_stop(void) {}
+
+static inline void ktsan_memblock_alloc(struct kmem_cache *cache, void *obj) {}
+static inline void ktsan_memblock_free(struct kmem_cache *cache, void *obj) {}
 
 static inline void ktsan_sync_acquire(void *addr) {}
 static inline void ktsan_sync_release(void *addr) {}
@@ -69,6 +75,12 @@ static inline void ktsan_sync_release(void *addr) {}
 static inline void ktsan_mtx_pre_lock(void *addr, bool write) {}
 static inline void ktsan_mtx_post_lock(void *addr, bool write) {}
 static inline void ktsan_mtx_pre_unlock(void *addr, bool write) {}
+
+static inline int ktsan_atomic32_read(const void *addr)
+{
+	return 0;
+}
+static inline void ktsan_atomic32_set(void *addr, int value) {}
 
 static inline void ktsan_read1(void *addr) {}
 static inline void ktsan_read2(void *addr) {}
@@ -83,9 +95,6 @@ static inline void ktsan_alloc_page(struct page *page, unsigned int order,
 		     gfp_t flags, int node) {}
 static inline void ktsan_free_page(struct page *page, unsigned int order) {}
 static inline void ktsan_split_page(struct page *page, unsigned int order) {}
-
-static inline void ktsan_memblock_alloc(struct kmem_cache *cache, void *obj) {}
-static inline void ktsan_memblock_free(struct kmem_cache *cache, void *obj) {}
 
 #endif /* CONFIG_KTSAN */
 
