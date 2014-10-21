55a72eb0813b7fed44f92ef7d627e38394ec7e35
Andrey Konovalov <adech.fo@gmail.com>
2014-10-21 14:50:50 +0400

    Intercept irq/preempt enable/disable events

---
+++ mm/ktsan/percpu.c
@@ -0,0 +1,43 @@
+#include "ktsan.h"
+
+#include <linux/irqflags.h>
+#include <linux/list.h>
+#include <linux/preempt.h>
+
+struct kt_percpu_sync_s {
+	uptr_t addr;
+	struct list_head list;
+};
+
+static void kt_percpu_track_enable(kt_thr_t *thr)
+{
+	thr->track_percpu = true;
+}
+
+static void kt_percpu_track_try_disable(kt_thr_t *thr)
+{
+	if (!irqs_disabled() && !preempt_count())
+		thr->track_percpu = false;
+
+	/* TODO(xairy): release all percpu syncs. */
+}
+
+void kt_preempt_disable(kt_thr_t *thr)
+{
+	kt_percpu_track_enable(thr);
+}
+
+void kt_preempt_enable(kt_thr_t *thr)
+{
+	kt_percpu_track_try_disable(thr);
+}
+
+void kt_irq_disable(kt_thr_t *thr)
+{
+	kt_percpu_track_enable(thr);
+}
+
+void kt_irq_enable(kt_thr_t *thr)
+{
+	kt_percpu_track_try_disable(thr);
+}
