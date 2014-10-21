3521426f39787241995351746c5e83f4593ec8a3
Andrey Konovalov <adech.fo@gmail.com>
2014-10-21 15:50:52 +0400

    Intercept percpu access

--- include/linux/percpu-defs.h
+++ include/linux/percpu-defs.h
@@ -16,6 +16,8 @@
 #ifndef _LINUX_PERCPU_DEFS_H
 #define _LINUX_PERCPU_DEFS_H
 
+#include <linux/ktsan.h>
+
 #ifdef CONFIG_SMP
 
 #ifdef MODULE
@@ -225,6 +227,7 @@ do {									\
 
 #define raw_cpu_ptr(ptr)						\
 ({									\
+	ktsan_percpu_acquire(ptr);					\
 	__verify_pcpu_ptr(ptr);						\
 	arch_raw_cpu_ptr(ptr);						\
 })
@@ -232,6 +235,7 @@ do {									\
 #ifdef CONFIG_DEBUG_PREEMPT
 #define this_cpu_ptr(ptr)						\
 ({									\
+	ktsan_percpu_acquire(ptr);					\
 	__verify_pcpu_ptr(ptr);						\
 	SHIFT_PERCPU_PTR(ptr, my_cpu_offset);				\
 })
