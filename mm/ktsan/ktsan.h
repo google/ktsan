a9442bbce7b5d2cb5caf8c9f8059ed857fb8f053
Andrey Konovalov <adech.fo@gmail.com>
2014-10-30 12:32:14 +0300

    Intercept atomic32 related functions

--- mm/ktsan/ktsan.h
+++ mm/ktsan/ktsan.h
@@ -327,8 +327,28 @@ void kt_mtx_pre_unlock(kt_thr_t *thr, uptr_t pc, uptr_t addr, bool wr);
 int kt_atomic32_read(kt_thr_t *thr, uptr_t pc, uptr_t addr);
 void kt_atomic32_set(kt_thr_t *thr, uptr_t pc, uptr_t addr, int value);
 
+void kt_atomic32_add(kt_thr_t *thr, uptr_t pc, uptr_t addr, int value);
+void kt_atomic32_sub(kt_thr_t *thr, uptr_t pc, uptr_t addr, int value);
+int kt_atomic32_sub_and_test(kt_thr_t *thr, uptr_t pc, uptr_t addr, int value);
+int kt_atomic32_add_negative(kt_thr_t *thr, uptr_t pc, uptr_t addr, int value);
+
+void kt_atomic32_inc(kt_thr_t *thr, uptr_t pc, uptr_t addr);
+void kt_atomic32_dec(kt_thr_t *thr, uptr_t pc, uptr_t addr);
+int kt_atomic32_inc_and_test(kt_thr_t *thr, uptr_t pc, uptr_t addr);
+int kt_atomic32_dec_and_test(kt_thr_t *thr, uptr_t pc, uptr_t addr);
+
 int kt_atomic32_read_no_ktsan(const void *addr);
 void kt_atomic32_set_no_ktsan(void *addr, int value);
+
+void kt_atomic32_add_no_ktsan(void *addr, int value);
+void kt_atomic32_sub_no_ktsan(void *addr, int value);
+int kt_atomic32_sub_and_test_no_ktsan(void *addr, int value);
+int kt_atomic32_add_negative_no_ktsan(void *addr, int value);
+
+void kt_atomic32_inc_no_ktsan(void *addr);
+void kt_atomic32_dec_no_ktsan(void *addr);
+int kt_atomic32_inc_and_test_no_ktsan(void *addr);
+int kt_atomic32_dec_and_test_no_ktsan(void *addr);
 
 /* Per-cpu synchronization. */
 
