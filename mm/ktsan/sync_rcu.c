64934936d882d98c248635c115d6965c4ffc8a18
Andrey Konovalov <adech.fo@gmail.com>
2014-11-19 19:04:02 +0300

    Fix rcu && add tests for rcu

---
+++ mm/ktsan/sync_rcu.c
@@ -0,0 +1,39 @@
+#include "ktsan.h"
+
+#include <linux/spinlock.h>
+
+void kt_rcu_read_lock(kt_thr_t *thr, uptr_t pc)
+{
+	kt_trace_add_event(thr, kt_event_type_rcu_read_lock, pc);
+	kt_clk_tick(&thr->clk, thr->id);
+}
+
+void kt_rcu_read_unlock(kt_thr_t *thr, uptr_t pc)
+{
+	kt_trace_add_event(thr, kt_event_type_rcu_read_unlock, pc);
+	kt_clk_tick(&thr->clk, thr->id);
+
+	spin_lock(&kt_ctx.rcu_lock);
+	kt_clk_acquire(thr, &kt_ctx.rcu_clk, &thr->clk);
+	spin_unlock(&kt_ctx.rcu_lock);
+}
+
+void kt_rcu_synchronize(kt_thr_t *thr, uptr_t pc)
+{
+	kt_trace_add_event(thr, kt_event_type_rcu_synchronize, pc);
+	kt_clk_tick(&thr->clk, thr->id);
+
+	spin_lock(&kt_ctx.rcu_lock);
+	kt_clk_acquire(thr, &thr->clk, &kt_ctx.rcu_clk);
+	spin_unlock(&kt_ctx.rcu_lock);
+}
+
+void kt_rcu_callback(kt_thr_t *thr, uptr_t pc)
+{
+	kt_trace_add_event(thr, kt_event_type_rcu_callback, pc);
+	kt_clk_tick(&thr->clk, thr->id);
+
+	spin_lock(&kt_ctx.rcu_lock);
+	kt_clk_acquire(thr, &thr->clk, &kt_ctx.rcu_clk);
+	spin_unlock(&kt_ctx.rcu_lock);
+}
