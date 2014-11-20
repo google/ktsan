a4174aab93a00ddbfaf0b2668177a3f10d29713b
Andrey Konovalov <adech.fo@gmail.com>
2014-11-20 13:35:33 +0300

    Intercept rcu_read_lock/unlock_bh/sched

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -923,6 +923,7 @@ static inline void rcu_read_lock_bh(void)
 	rcu_lock_acquire(&rcu_bh_lock_map);
 	rcu_lockdep_assert(rcu_is_watching(),
 			   "rcu_read_lock_bh() used illegally while idle");
+	ktsan_rcu_read_lock_bh();
 }
 
 /*
@@ -932,6 +933,7 @@ static inline void rcu_read_lock_bh(void)
  */
 static inline void rcu_read_unlock_bh(void)
 {
+	ktsan_rcu_read_unlock_bh();
 	rcu_lockdep_assert(rcu_is_watching(),
 			   "rcu_read_unlock_bh() used illegally while idle");
 	rcu_lock_release(&rcu_bh_lock_map);
@@ -959,6 +961,7 @@ static inline void rcu_read_lock_sched(void)
 	rcu_lock_acquire(&rcu_sched_lock_map);
 	rcu_lockdep_assert(rcu_is_watching(),
 			   "rcu_read_lock_sched() used illegally while idle");
+	ktsan_rcu_read_lock_sched();
 }
 
 /* Used by lockdep and tracing: cannot be traced, cannot call lockdep. */
@@ -966,6 +969,7 @@ static inline notrace void rcu_read_lock_sched_notrace(void)
 {
 	preempt_disable_notrace();
 	__acquire(RCU_SCHED);
+	ktsan_rcu_read_lock_sched();
 }
 
 /*
@@ -975,6 +979,7 @@ static inline notrace void rcu_read_lock_sched_notrace(void)
  */
 static inline void rcu_read_unlock_sched(void)
 {
+	ktsan_rcu_read_unlock_sched();
 	rcu_lockdep_assert(rcu_is_watching(),
 			   "rcu_read_unlock_sched() used illegally while idle");
 	rcu_lock_release(&rcu_sched_lock_map);
@@ -985,6 +990,7 @@ static inline void rcu_read_unlock_sched(void)
 /* Used by lockdep and tracing: cannot be traced, cannot call lockdep. */
 static inline notrace void rcu_read_unlock_sched_notrace(void)
 {
+	ktsan_rcu_read_unlock_sched();
 	__release(RCU_SCHED);
 	preempt_enable_notrace();
 }
