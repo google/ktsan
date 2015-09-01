8b0d4f0f06961bb4706f52de1ae6545bd7f74e81
Andrey Konovalov <andreyknvl@google.com>
2015-09-01 22:37:21 +0200

    Fix trace dump

--- mm/ktsan/trace.c
+++ mm/ktsan/trace.c
@@ -124,7 +124,7 @@ void kt_trace_dump(kt_trace_t *trace, uptr_t beg, uptr_t end)
 	uptr_t pc;
 
 	for (i = beg; i <= end; i++) {
-		event = &trace->events[i];
+		event = &trace->events[i % KT_TRACE_SIZE];
 		if (event->type == kt_event_type_func_enter) {
 			pc = kt_pc_decompress(event->data);
 			pr_err(" i: %lu, enter  , pc: [<%p>] %pS\n",
